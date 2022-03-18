b4ebd563aa0ea0a2f30e0ee0e40df2e964dea6c4
Murtuza <34296103+murtazaalam@users.noreply.github.com>
2022-03-19 01:25:49 +0530

    same error

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ const dbConnection = require("./config/db.config");
 const router = require("./routes");
 const app = express();
 const cors = require("cors");
-const PORT = 8080;
+const PORT = 7789;
 
 app.use(cors());
 
-dbConnection((response) => {
-  if (response) process.exit(1);
-  console.log("Db connection success");
-});
+
 app.use(express.json());
 app.use("/v1", router);
 app.get("/", (req, res, next) => {
   return res.jsonp({ error: 0, message: "OK" });
+});
+dbConnection((response) => {
+  if (response) process.exit(1);
+  console.log("Db connection success");
 });
 app.listen(PORT, () => {
   console.log(`server is up at ${PORT}`);
